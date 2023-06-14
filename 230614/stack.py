6f26bff56c11227f9901b426b0cdef77829e1654
기남석 <kee0304@gmail.com>
2023-06-14 10:19:36 +0900

    230614 ver.2

--- 230614/stack.py
+++ 230614/stack.py
@@ -1,15 +1,13 @@
-import collections
-
 T = int(input())
 
 for t in range(1, T+1):
-    inputlist = collections.deque(input())
+    inputlist = list(input())[::-1]
     stack = []      # 괄호를 담을 스택
     cnt = 0         # 봉의 개수를 저장할 변수
     forgwal = None  # 앞의 괄호를 기억할 변수
 
     while len(inputlist) >=1:       # 입력된 리스트에서 하나씩 빼면서 진행
-        gwal = inputlist.popleft()
+        gwal = inputlist.pop()
         if gwal == "(":             # 여는 괄호면
             stack.append(gwal)      # 그냥 넣고
             forgwal = gwal          # 저장
