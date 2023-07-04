3c4803ca32d415fa8cdb1eb49777a2eb21563692
기남석 <kee0304@gmail.com>
2023-07-04 09:58:37 +0900

    230704 230703finish

--- 230703/230723.py
+++ 230703/230723.py
@@ -1,18 +1,25 @@
 import itertools
 
+# 
 def honey(row, col,C):
     global ans
+    
+    # 작업자 1의 꿀통
+    worker1=mat[row][col:col+M]
+    comb1 = []
+    
+    # 가능한 꿀통 조합을 만들고 가능한 큰 꿀 채취량 구하기
+    for length in range(1, M+1):
+        comb1.extend(list(itertools.combinations(worker1,length)))
+        worker1max = searchPart(comb1, C)
 
+    # 작업자 2의 꿀통 탐색
     for subRow in range(row, N):
+        # 같은 줄에 있는 경우
         if subRow == row:
+            # 범위 제한
             if N-(col+M) >= M:
-                worker1 = mat[subRow][col:col+M]
-                comb1 = []
-                for length in range(1, M+1):
-                    comb1.extend(list(itertools.combinations(worker1,length)))
-                worker1max = searchPart(comb1, C)
-
-                for subCol in range(N-(col+M),N-M):
+                for subCol in range(N-(col+M),N-M+1):
                     worker2 = mat[subRow][subCol:subCol+M]
                     comb2 = []
                     for length in range(1,M+1):
@@ -21,15 +28,10 @@ def honey(row, col,C):
 
                     if worker1max + worker2max > ans:
                         ans = worker1max + worker2max
-                
-        else:
-            worker1 = mat[subRow][col:col+M]
-            comb1 = []
-            for length in range(1, M+1):
-                comb1.extend(list(itertools.combinations(worker1,length)))
-            worker1max = searchPart(comb1, C)
 
-            for subCol in range(N-M):
+        # 다른 줄에 있는 경우
+        else:
+            for subCol in range(N-M+1):
                 worker2 = mat[subRow][subCol:subCol+M]
                 comb2 = []
                 for length in range(1,M+1):
@@ -37,12 +39,10 @@ def honey(row, col,C):
                 worker2max = searchPart(comb2, C)
 
 
-                print(comb1, comb2)
                 if worker1max + worker2max > ans:
                     ans = worker1max + worker2max
 
-
-
+# 조합 중에서 가능한 가장 큰 채취량을 가지는 경우의 수 찾기
 def searchPart(comb,C):
     max = 0
     for part in comb:
@@ -67,8 +67,10 @@ for t in range(1, T+1):
 
     ans = 0
 
+    # 작업자 1의 꿀통 시작 위치
     for row in range(N):
         for col in range(N-M+1):
+            # 가능한 경우의 수 탐색
             honey(row, col, C)
 
     print(f'#{t} {ans}')
